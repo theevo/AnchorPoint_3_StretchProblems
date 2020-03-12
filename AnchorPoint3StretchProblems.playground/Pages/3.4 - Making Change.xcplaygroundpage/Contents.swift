4f6b79461a02be1ce43246a5380b7033eef14f35
Theo Vora <theo.vora@gmail.com>
2020-03-12 12:19:53 -0600

    complete 3.4

--- AnchorPoint3StretchProblems.playground/Pages/3.4 - Making Change.xcplaygroundpage/Contents.swift
+++ AnchorPoint3StretchProblems.playground/Pages/3.4 - Making Change.xcplaygroundpage/Contents.swift
@@ -14,5 +14,64 @@
 
 import Foundation
 
+enum Denominations: Float {
+    case penny = 0.01
+    case nickle = 0.05
+    case dime = 0.1
+    case quarter = 0.25
+    case dollar = 1.00
+}
 
+func coinCounter(for amount: Float, coinValue: Float) -> (remainder: Float, coinCount: Float) {
+    let coinQty = (amount / coinValue).rounded(.down)
+    var remainder = amount.truncatingRemainder(dividingBy: coinValue)
+    
+    // === Special Case ===
+    // after the nickles count is determined, pennies are next.
+    // we must return an exact remainder (0.01 - 0.04) in order for the pennies call to complete successfully
+    
+    if coinValue == Denominations.nickle.rawValue {
+        remainder = round(100*remainder)/100
+        print(remainder)
+    }
+    return (remainder, coinQty)
+}
+
+func makeChange(for amount: Float) -> String {
+    
+    let dollars = coinCounter(for: amount, coinValue: Denominations.dollar.rawValue)
+    let dollarQty = dollars.coinCount
+    let coinAmount = dollars.remainder
+    
+    let quarters = coinCounter(for: coinAmount, coinValue: Denominations.quarter.rawValue)
+    let quarterQty = quarters.coinCount
+    let quarterRemainder = quarters.remainder
+    
+    let dimes = coinCounter(for: quarterRemainder, coinValue: Denominations.dime.rawValue)
+    let dimeQty = dimes.coinCount
+    let dimeRemainder = dimes.remainder
+    
+    let nickles = coinCounter(for: dimeRemainder, coinValue: Denominations.nickle.rawValue)
+    let nickleQty = nickles.coinCount
+    let nickleRemainder = nickles.remainder
+    
+    let pennies = coinCounter(for: nickleRemainder, coinValue: Denominations.penny.rawValue)
+    let pennyQty = pennies.coinCount
+    
+    
+    return """
+    $\(amount) becomes...
+    \t \(dollarQty) dollars
+    \t \(quarterQty) quarters
+    \t \(dimeQty) dimes
+    \t \(nickleQty) nickles
+    \t \(pennyQty) pennies
+    """
+}
+
+//print(makeChange(for: 0.01))
+//print(makeChange(for: 2.15))
+print(makeChange(for: 2.51))
+print(makeChange(for: 2.52))
+print(makeChange(for: 212.59))
 //: [Next](@next)
